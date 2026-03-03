173744479f4a309180987e605498d36e036a619b
Andriamihaja <andoandria2386@gmail.com>
2026-03-03 07:47:14 +0300

    Update cartController.js

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,7 +1,7 @@
 const Cart = require('../models/client/Cart');
 const Product = require('../models/boutique/Products');
 const Order = require('../models/boutique/Order');
-const { addNotification } = require('./notificationController');
+const { addNotification } = require('./NotificationController');
 
 // Ajouter un produit au panier
 exports.addToCart = async (req, res) => {
