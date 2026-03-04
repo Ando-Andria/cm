bde9786d7ba343e50d4769330dd0b13a543903e2
Andriamihaja <andoandria2386@gmail.com>
2026-03-04 17:23:30 +0300

    login customer

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,5 +1,6 @@
 const Customer = require('../models/boutique/Customer');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 // Créer un client
 exports.createCustomer = async (req, res) => {
   try {
@@ -19,7 +20,73 @@ exports.getCustomers = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
- 
+ exports.registerCustomer = async (req, res) => {
+  try {
+    const { name, email, password, phone, address, shopId } = req.body;
+
+    // Vérifier si email existe déjà
+    const existing = await Customer.findOne({ email });
+    if (existing) {
+      return res.status(400).json({ error: 'Email déjà utilisé' });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const customer = await Customer.create({
+      name,
+      email,
+      password: hashedPassword,
+      phone,
+      address,
+      shopId
+    });
+
+    res.status(201).json({
+      message: 'Client créé avec succès',
+      customerId: customer._id
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// ================= LOGIN =================
+exports.loginCustomer = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const customer = await Customer.findOne({ email });
+    if (!customer) {
+      return res.status(400).json({ error: 'Email ou mot de passe incorrect' });
+    }
+
+    // Vérifier mot de passe
+    const isMatch = await bcrypt.compare(password, customer.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Email ou mot de passe incorrect' });
+    }
+
+    // Créer token
+    const token = jwt.sign(
+      { id: customer._id, shopId: customer.shopId },
+      process.env.JWT_SECRET,
+      { expiresIn: '7d' }
+    );
+
+    res.status(200).json({
+      message: 'Connexion réussie',
+      token,
+      userId: customer._id,
+      shopId: customer.shopId
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
 exports.getCustomersByShop = async (req, res) => {
   try {
     const { shopId } = req.params;
--- models/boutique/Customer.js
+++ models/boutique/Customer.js
@@ -4,8 +4,9 @@ const customerSchema = new mongoose.Schema({
   name: { type: String, required: true },           
   email: { type: String, unique: true, sparse: true }, 
   phone: { type: String },
+  password: { type: String ,required: true},
   address: { type: String },
-  shopId: { type: mongoose.Schema.Types.ObjectId, ref: 'Shop',required: true },
+  shopId: { type: mongoose.Schema.Types.ObjectId, ref: 'Shop' },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Customer', customerSchema);
