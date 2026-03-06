068f9bbdb3545c18e9e4b8752d458ea575c24ba8
Andriamihaja <andoandria2386@gmail.com>
2026-03-06 13:21:36 +0300

    modif login customer

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
-
+const customerController = require('../controllers/customerController');
 router.post('/register', register);
 router.post('/login', login);
+router.post('/registerClient', customerController.registerCustomer);
+router.post('/loginClient', customerController.loginCustomer);
 
 module.exports = router;
